45fd068b80c0abdcefb14f255649f8692f17a5b5
Josh Powell <josh.powell@softwire.com>
2019-11-12 19:08:23 +0000

    randomise phrases for QA bot

--- index.js
+++ index.js
@@ -44,6 +44,18 @@ const QAers = [
     }
 ];
 
+const messages =[
+    "@@@ is the QAer for the job",
+    "@@@ It's your lucky day, time to do some QA!",
+    "eenie meenie minie moe I choose @@@ to do some QA",
+    "QA BOT HATH SPOKEN: @@@ shall do the QA",
+    "@@@ is the QAer you're looking for",
+    "I think @@@ seems in the mood to do some QA",
+    "I spy with my little eye: @@@ is about to do some QA",
+    "Could @@@ BE about to do any more QA?",
+    "@@@ here's a present: Some QA!",
+]
+
 const rando = Math.floor(Math.random() * QAers.length);
 
 
@@ -51,15 +63,16 @@ app.post('/qa', (req, res) => {
 
     const sendingUserId = req.body.user_id;
     const filteredQa = QAers.filter(q => q.id !== sendingUserId);
-    const randomNumber = Math.random(filteredQa.length);
-    const Qaer = QAers[randomNumber];
+    const randomNumber = Math.floor(Math.random() * filteredQa.length);
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    const Qaer = filteredQa[randomNumber];
+    const userId = `<@${Qaer.id}>`;
+    const updatedMessage = randomMessage.replace('@@@', userId)
     const response = {
         response_type: "in_channel",
-        text: `<@${Qaer}> is the guy for you!}`
+        text: updatedMessage
     };
-    console.log(response);
     res.send(response);
-    //axios.post(req.body.response_url, {text: `${developers[rando]} is the guy for you`})
 
 })
 
