e31930f8f4411ef7ae390da47c5da528a5c74e64
Josh Powell <josh.powell@softwire.com>
2019-11-12 17:04:33 +0000

    Add qa response

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import express from 'express';
 import axios from 'axios';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 const PORT = process.env.PORT || 5000
+app.use(bodyParser);
 
 app.get('/helloWorld', (req, res) => {
     res.send('Hello world!');
 })
+
+const developers = [
+    "Josh",
+    "Robin",
+    "Nathan"
+];
+const rando = Math.floor(Math.random() * developers.length);
+
+
+app.post('/qa', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+    axios.post(req.body.response_url, {text: `${developers[rando]} is the guy for you`})
+
+})
+
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
