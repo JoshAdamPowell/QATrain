cac8d9d74b7842ae3cde8cbdb73e222048caa4a4
Josh Powell <josh.powell@softwire.com>
2019-11-13 12:31:59 +0000

    Add standup options

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import axios from 'axios';
 import bodyParser from 'body-parser';
 
 
@@ -13,34 +12,60 @@ app.get('/helloWorld', (req, res) => {
     res.send('Hello world!');
 })
 
-const QAers = [
+const people = [
     {
         name: "Josh",
-        id: "UG3AZUDR9"
+        id: "UG3AZUDR9",
+        isQaer: true,
+        isSoftwire: true
     },
     {
         name: "Robin",
-        id: "UNZJ1235L"
+        id: "UNZJ1235L",
+        isQaer: true,
+        isSoftwire: true
     },
     {
         name: "Nathan",
-        id: "UPB3V5EN7"
+        id: "UPB3V5EN7",
+        isQaer: true,
+        isSoftwire: true
     },
     {
         name: "Ray",
-        id: "UF87QFDAL"
+        id: "UF87QFDAL",
+        isQaer: true,
+        isSoftwire: false
     },
     {
         name: "Esam",
-        id: "UF8BM027K"
+        id: "UF8BM027K",
+        isQaer: true,
+        isSoftwire: false
     },
     {
         name: "Konrad",
-        id: "UN6RMRXB3"
+        id: "UN6RMRXB3",
+        isQaer: true,
+        isSoftwire: false
     },
     {
         name: "Thomas",
-        id: "U2XH996F3"
+        id: "U2XH996F3",
+        isQaer: true,
+        isSoftwire: false
+    },
+    {
+        name: "Pramila",
+        id: "U3CUJ3Q56",
+        isQaer: false,
+        isSoftwire: false
+    },
+    {
+        name: "James",
+        id: "U8U9U7588",
+        isQaer: false,
+        isSoftwire: false
     }
 ];
 
@@ -48,21 +73,28 @@ const messages =[
     "@@@ is the QAer for the job",
     "@@@ It's your lucky day, time to do some QA!",
     "eenie meenie minie moe I choose @@@ to do some QA",
-    "QA BOT HATH SPOKEN: @@@ shall do the QA",
+    "QA BOT HATH SPOKEN: @@@ shalt do the QA",
     "@@@ is the QAer you're looking for",
     "I think @@@ seems in the mood to do some QA",
     "I spy with my little eye: @@@ is about to do some QA",
     "Could @@@ BE about to do any more QA?",
     "@@@ here's a present: Some QA!",
-]
+    "Choo Choo! @@@ is all aboard the QA train!"
+];
 
-const rando = Math.floor(Math.random() * QAers.length);
+const standupMessages = [
+    "@@@ will be walking the board today",
+    "Rejoice! @@@ is about to walk us through the board",
+    "The board is about to be walked by @@@",
+];
+
+const rando = Math.floor(Math.random() * people.length);
 
 
 app.post('/qa', (req, res) => {
 
     const sendingUserId = req.body.user_id;
-    const filteredQa = QAers.filter(q => q.id !== sendingUserId);
+    const filteredQa = people.filter(q => q.id !== sendingUserId && q.isQaer);
     const randomNumber = Math.floor(Math.random() * filteredQa.length);
     const randomMessage = messages[Math.floor(Math.random() * messages.length)];
     const Qaer = filteredQa[randomNumber];
@@ -74,6 +106,23 @@ app.post('/qa', (req, res) => {
     };
     res.send(response);
 
+})
+
+app.post('/standup', (req, res) => {
+    const dayOfWeek = new Date().getDay();
+    const isItFridayOrMonday = dayOfWeek === 1 || dayOfWeek === 5;
+    const isItFridayOrMonday = true;
+    const standupers = people.filter(p => !isItFridayOrMonday || !p.isSoftwire)
+    const randomNumber = Math.floor(Math.random() * standupers.length);
+    const standuper = standupers[randomNumber];
+    const userId = `<@${standuper.id}>`;
+    const randomMessage = standupMessages[Math.floor(Math.random() * standupMessages.length)];
+    const updatedMessage = randomMessage.replace('@@@', userId) + standuper.name;
+    const response = {
+        response_type: "in_channel",
+        text: updatedMessage
+    };
+    res.send(response);
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
